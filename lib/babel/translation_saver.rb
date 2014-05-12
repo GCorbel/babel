33598a3b996d3be7cd1ed8f049bf8aaad58f808c
GCorbel <guirec.corbel@gmail.com>
2014-05-12 05:56:56 -0400

    use a correct namespace

--- lib/babel/translation_saver.rb
+++ lib/babel/translation_saver.rb
@@ -1,34 +1,36 @@
-class TranslationSaver
-  def initialize(hash)
-    @hash = hash
-  end
-
-  def call
-    store_translation_in_memory
-    write_in_file
-  end
-
-  private
-  def store_translation_in_memory
-    I18n.backend.store_translations(I18n.locale, @hash)
-  end
-
-  def write_in_file
-    path = "#{Rails.root}/config/locales/#{I18n.locale}.yml"
-    exisiting_locales = YAML.load(File.read(path))
-    full_locales = deep_merge(exisiting_locales, @hash)
-    File.open(path, 'w') { |f| f.write(full_locales.to_yaml) }
-  end
-
-  def deep_merge(first_hash, other_hash, &block)
-    other_hash.each_pair do |current_key, other_value|
-      this_value = first_hash[current_key]
-      first_hash[current_key] = if this_value.is_a?(Hash) && other_value.is_a?(Hash)
-                                  deep_merge(this_value, other_value, &block)
-                                else
-                                  other_value
-                                end
+module Babel
+  class TranslationSaver
+    def initialize(hash)
+      @hash = hash
     end
-    first_hash
+
+    def call
+      store_translation_in_memory
+      write_in_file
+    end
+
+    private
+    def store_translation_in_memory
+      I18n.backend.store_translations(I18n.locale, @hash)
+    end
+
+    def write_in_file
+      path = "#{Rails.root}/config/locales/#{I18n.locale}.yml"
+      exisiting_locales = YAML.load(File.read(path))
+      full_locales = deep_merge(exisiting_locales, @hash)
+      File.open(path, 'w') { |f| f.write(full_locales.to_yaml) }
+    end
+
+    def deep_merge(first_hash, other_hash, &block)
+      other_hash.each_pair do |current_key, other_value|
+        this_value = first_hash[current_key]
+        first_hash[current_key] = if this_value.is_a?(Hash) && other_value.is_a?(Hash)
+                                    deep_merge(this_value, other_value, &block)
+                                  else
+                                    other_value
+                                  end
+      end
+      first_hash
+    end
   end
 end
--- spec/lib/translation_saver_spec.rb
+++ spec/lib/translation_saver_spec.rb
@@ -1,12 +1,14 @@
 require 'spec_helper'
 
-describe TranslationSaver do
-  describe "#call" do
-    it "save to the file" do
-      FileUtils.cp('spec/fixtures/en.yml', 'spec/dummy/config/locales/')
-      hash = { "node1" => { "node2" => "value" } }
-      TranslationSaver.new(hash).call
-      expect(I18n.t('node1.node2')).to eq "value"
+module Babel
+  describe TranslationSaver do
+    describe "#call" do
+      it "save to the file" do
+        FileUtils.cp('spec/fixtures/en.yml', 'spec/dummy/config/locales/')
+        hash = { "node1" => { "node2" => "value" } }
+        TranslationSaver.new(hash).call
+        expect(I18n.t('node1.node2')).to eq "value"
+      end
     end
   end
 end
